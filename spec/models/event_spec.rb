cd235ddfd58c2fada8f6644f8d78c63b0bef905d
WILLEM SINNIGE <W.SINNIGE@GMAIL.COM>
2017-09-06 19:11:12 +0200

    Add Model Specs: Associations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -23,14 +23,44 @@ RSpec.describe Event, type: :model do
 #     end
 #   end
 
-describe ".order_by_price" do
-    let!(:event1) { create :event, price: 100 }
-    let!(:event2) { create :event, price: 200 }
-    let!(:event3) { create :event, price: 50 }
+# describe ".order_by_price" do
+#     let!(:event1) { create :event, price: 100 }
+#     let!(:event2) { create :event, price: 200 }
+#     let!(:event3) { create :event, price: 50 }
+#
+#     it "returns a sorted array of events by prices" do
+#       # note that they should not come out in the order that they were created
+#       expect(Event.order_by_price).to eq([event3, event1, event2])
+#     end
+#   end
+# end
 
-    it "returns a sorted array of events by prices" do
-      # note that they should not come out in the order that they were created
-      expect(Event.order_by_price).to eq([event3, event1, event2])
-    end
+describe "association with user" do
+  let(:user) { create :user }
+
+  it "belongs to a user" do
+    event = user.events.new(name: "Shared")
+
+    expect(event.user).to eq(user)
   end
+
+  # OR, go the shoulda way:
+
+    it { is_expected.to belong_to :user}
+    # and an added bonus: test the categories association in one line as well:
+    it { is_expected.to have_and_belong_to_many :categories }
+end
+describe "association with category" do
+  let(:event) { create :event }
+
+  let(:category1) { create :category, name: "Bright", events: [event] }
+  let(:category2) { create :category, name: "Clean lines", events: [event] }
+  let(:category3) { create :category, name: "A Man's Touch", events: [event] }
+
+  it "has categories" do
+    expect(event.categories).to include(category1)
+    expect(event.categories).to include(category2)
+    expect(event.categories).to include(category3)
+  end
+end
 end
